138294ff0b114a3e4f8bf5c4df164f3eaecd180e
vandej26 <joris.vandenbossche@infofarm.be>
2016-09-21 15:09:20 +0200

    added comment

--- ui.R
+++ ui.R
@@ -13,6 +13,10 @@ shinyUI(fluidPage(
   # Sidebar with a checkbox for country and checkbox for statistic
   sidebarLayout(
     sidebarPanel(
+      "This app works with a data set that was taken from the IMF website.
+      Below you can choose a country and the available statistics will be loaded
+      automatically into the second selection box. The app filters the data according
+      to the chosen options and will display a graph for you!",
       # Options
       selectInput("country", "Country or region:", CountryChoices, 
                   selected = "World", multiple = FALSE, selectize = TRUE),
