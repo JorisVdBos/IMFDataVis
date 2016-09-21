2540e8676ab6a3438d5c324eb9846c4ad80eaf60
vandej26 <joris.vandenbossche@infofarm.be>
2016-09-21 11:04:55 +0200

    Plotting

---
+++ plotFunc.R
@@ -0,0 +1,15 @@
+plotInfo(country, statistic){
+  info <- IFS[Country.Name == country & Indicator.Name == statistic & Attribute != "Value"]
+  if(dim(info)[1] == 0) return(NULL)
+  col <- which(!is.na(t(info)[7:210]))[1] + 6
+  info <- info[, c(6, col), with = FALSE]
+  names(info)[1] <- "Plot Info"
+  names(info)[2] <- ""
+  info
+}
+
+makeTs(country, statistic){
+  data <- IFS[Country.Name == country & Indicator.Name == statistic & Attribute == "Value",]
+  data <- ts(t(data)[7:210], start = 2000, frequency = 12)
+  data
+}
